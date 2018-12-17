e89b0056566b1556368ff6fc8a2bc271afc3a0e9
Michael Katan <michaelkatan@gmail.com>
2018-12-17 09:11:22 +0200

    Initial commit

---
+++ src/models/Activity.java
@@ -0,0 +1,11 @@
+package models;
+
+public class Activity
+{
+    private int id;
+    private String name;
+    private boolean hasSets;
+    private int numberOfSets;
+
+
+}
---
+++ src/models/RequestListener.java
@@ -0,0 +1,8 @@
+package models;
+
+public interface RequestListener <T>
+{
+    void onComplete(T t);
+    void onError(String errorMsg);
+
+}
